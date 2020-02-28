f4aa8ea75cc61192c6e9e56f44dab8d319889cce
David Burke <david@burkesoftware.com>
2020-02-27 21:57:24 -0500

    Added teams api

--- teams/models.py
+++ teams/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 
 class Team(models.Model):
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
     slug = models.SlugField()
     organization = models.ForeignKey(
         "organizations.Organization", on_delete=models.CASCADE, related_name="teams"
     )
     members = models.ManyToManyField("users.User")
     projects = models.ManyToManyField("projects.Project")
+
+    class Meta:
+        unique_together = ("slug", "organization")
