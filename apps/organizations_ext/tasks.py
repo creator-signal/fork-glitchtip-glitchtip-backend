434f1bf55e434377a9c06ee4ba18325fe8d90dbd
David Burke <david@burkesoftware.com>
2025-12-27 12:10:20 -0500

    Async send

--- apps/organizations_ext/tasks.py
+++ apps/organizations_ext/tasks.py
@@ -1,3 +1,4 @@
+from asgiref.sync import sync_to_async
 from django.core.cache import cache
 from django.tasks import task
 
@@ -6,29 +7,31 @@ from .models import Organization
 
 
 @task
-def check_organization_throttle(organization_id: int, bypass_cache: bool = False):
-    if not bypass_cache and not cache.add(f"org-throttle-{organization_id}", True):
+async def check_organization_throttle(organization_id: int, bypass_cache: bool = False):
+    if not bypass_cache and not await cache.aadd(
+        f"org-throttle-{organization_id}", True
+    ):
         return  # Recent check already performed
 
-    org = (
+    org = await (
         Organization.objects.with_event_counts()
         .select_related("stripe_primary_subscription__price__product")
-        .get(id=organization_id)
+        .aget(id=organization_id)
     )
-    _check_and_update_throttle(org)
+    await _check_and_update_throttle(org)
 
 
 @task
-def check_all_organizations_throttle():
-    for org in (
+async def check_all_organizations_throttle():
+    async for org in (
         Organization.objects.with_event_counts()
         .select_related("stripe_primary_subscription__price__product")
-        .iterator()
+        .aiterator()
     ):
-        _check_and_update_throttle(org)
+        await _check_and_update_throttle(org)
 
 
-def _check_and_update_throttle(org: Organization):
+async def _check_and_update_throttle(org: Organization):
     plan_events: int | None = None
     if org.stripe_primary_subscription:
         plan_events = org.stripe_primary_subscription.price.product.events
@@ -43,16 +46,16 @@ def _check_and_update_throttle(org: Organization):
     if org.event_throttle_rate != org_throttle:
         old_throttle = org.event_throttle_rate
         org.event_throttle_rate = org_throttle
-        org.save(update_fields=["event_throttle_rate"])
+        await org.asave(update_fields=["event_throttle_rate"])
         if org_throttle > old_throttle:
-            send_throttle_email.enqueue(org.id)
+            await send_throttle_email.aenqueue(org.id)
 
 
 @task
-def send_throttle_email(organization_id: int):
-    ThrottleNoticeEmail(pk=organization_id).send_email()
+async def send_throttle_email(organization_id: int):
+    await sync_to_async(ThrottleNoticeEmail(pk=organization_id).send_email)()
 
 
 @task
-def send_email_invite(org_user_id: int, token: str):
-    InvitationEmail(pk=org_user_id, token=token).send_email()
+async def send_email_invite(org_user_id: int, token: str):
+    await sync_to_async(InvitationEmail(pk=org_user_id, token=token).send_email)()
