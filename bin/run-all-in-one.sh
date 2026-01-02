5e9fb2afb530246d1dc5586f36637e2ed715c445
David Burke <david@burkesoftware.com>
2026-01-02 13:56:51 -0500

    Remove app.py, in factor of bash script

--- bin/run-all-in-one.sh
+++ bin/run-all-in-one.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env sh
+set -e
 
-exec python app.py
+# Run initialization commands
+python manage.py migrate --no-input --skip-checks
+python manage.py pgpartition --yes
+
+# Create cache table if django.contrib.sessions is installed
+python -c "import os, django; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'glitchtip.settings'); django.setup(); from django.conf import settings; from django.core.management import call_command; call_command('createcachetable') if 'django.contrib.sessions' in settings.INSTALLED_APPS else None"
+
+# Enable embedded worker
+export GLITCHTIP_EMBED_WORKER=true
+
+# Granian settings
+
+WORKERS=${WEB_CONCURRENCY:-1}
+
+HOST=${GRANIAN_HOST:-0.0.0.0}
+
+PORT=${GRANIAN_PORT:-8000}
+
+G_LOG_LEVEL=${GRANIAN_LOG_LEVEL:-INFO}
+
+
+
+# Run Granian
+
+exec granian --interface asgi glitchtip.asgi:application --host $HOST --port $PORT --workers $WORKERS --log-level $G_LOG_LEVEL --no-ws
