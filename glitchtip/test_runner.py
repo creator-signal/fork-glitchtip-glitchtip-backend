1d9fdf4ee1586aa2e9d62c8836daed35b0b2b9c9
David Burke <david@burkesoftware.com>
2026-01-21 14:08:33 -0500

    Test Runner: Create default partitions during test setup to support model_bakery/freezegun

--- glitchtip/test_runner.py
+++ glitchtip/test_runner.py
@@ -34,3 +34,28 @@ class TimedTestRunner(DiscoverRunner):
     """To view timings, use ./manage.py test -v 2"""
 
     test_runner = TimedTextTestRunner
+
+    def setup_databases(self, **kwargs):
+        result = super().setup_databases(**kwargs)
+        self._create_test_default_partitions()
+        return result
+
+    def _create_test_default_partitions(self):
+        from django.db import connection
+
+        tables = [
+            "issue_events_issueevent",
+            "issue_events_issueaggregate",
+            "issue_events_issuetag",
+            "performance_transactionevent",
+            "performance_transactiongroupaggregate",
+            "uptime_monitorcheck",
+            "projects_issueeventprojecthourlystatistic",
+            "projects_transactioneventprojecthourlystatistic",
+        ]
+
+        with connection.cursor() as cursor:
+            for table in tables:
+                cursor.execute(
+                    f"CREATE TABLE IF NOT EXISTS {table}_default PARTITION OF {table} DEFAULT;"
+                )
